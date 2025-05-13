6a751ebfd259df9d10cd86f716f1ccf158d7ca45
ShockCharge <48907444+ShockCharge@users.noreply.github.com>
2025-05-13 15:44:02 +1200

    Test 3

--- Source/FPS_205/Private/MonsterStats.cpp
+++ Source/FPS_205/Private/MonsterStats.cpp
@@ -1,34 +1,32 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "MonsterStats.h"
+//#include "MonsterStats.h"
 
 // Sets default values
-AMonsterStats::AMonsterStats()
-{
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+//UMonsterStats::UMonsterStats()
+//{
+// 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+//	PrimaryActorTick.bCanEverTick = true;
+//
+//}
+//
+//// Called when the game starts or when spawned
+//void UMonsterStats::BeginPlay()
+//{
+//	Super::BeginPlay();
+//	
+//}
+//
+//// Called every frame
+//void UMonsterStats::Tick(float DeltaTime)
+//{
+//	Super::Tick(DeltaTime);
+//
+//}
 
-}
-
-// Called when the game starts or when spawned
-void AMonsterStats::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void AMonsterStats::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void AMonsterStats::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
-
+//// Called to bind functionality to input
+//void UMonsterStats::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+//{
+//	Super::SetupPlayerInputComponent(PlayerInputComponent);
+//}
